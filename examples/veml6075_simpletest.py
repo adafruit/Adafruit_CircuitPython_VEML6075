89fff9e65629161a52f6f23334b69869803a32a3
foamyguy <foamyguy@gmail.com>
2026-04-23 13:53:27 -0500

    "Update ruff pre-commit hook "

--- examples/veml6075_simpletest.py
+++ examples/veml6075_simpletest.py
@@ -12,7 +12,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 veml = adafruit_veml6075.VEML6075(i2c, integration_time=100)
 
-print("Integration time: %d ms" % veml.integration_time)
+print(f"Integration time: {veml.integration_time} ms")
 
 while True:
     print(veml.uv_index)
